e50fce53d6d565ef94f6f281088ae7c0923b8870
osmanHancer <ohancer74@gmail.com>
2025-11-04 20:19:08 +0300

    telefon ve insta adresi eklendi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,19 @@ import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
-  { path: 'login', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
-  { path: 'dashboard', canActivate: [authGuard], loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent) },
-  { path: 'persons', canActivate: [authGuard], loadComponent: () => import('./pages/persons/persons.component').then(m => m.PersonsComponent) },
-
-  // 👇 Artık korumasız
+  { path: '', pathMatch: 'full', redirectTo: 'persons' },
+  { 
+    path: 'login', 
+    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) 
+  },
+  { 
+    path: 'persons', 
+    canActivate: [authGuard], 
+    loadComponent: () => import('./pages/persons/persons.component').then(m => m.PersonsComponent) 
+  },
   { 
     path: 'person/:tcno',
     loadComponent: () => import('./pages/person/person').then(m => m.Person)
   },
-
-  { path: '**', redirectTo: 'dashboard' },
+  { path: '**', redirectTo: 'persons' },
 ];
